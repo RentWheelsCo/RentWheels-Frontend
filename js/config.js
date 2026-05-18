e9bed8b7a3160ca2e9e0da6789dba9796f778938
Jinisha Shrestha <jinishashr7@gmail.com>
2026-05-18 22:33:03 +0545

    fix: Vehicle Status chnage

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@ window.RW_CONFIG = window.RW_CONFIG || {};
 const RW_LOCAL_API = "http://localhost:5000/api";
 const RW_REMOTE_API = "https://rentwheels-zr2p.onrender.com/api";
 
-window.RW_CONFIG.API_BASE = RW_REMOTE_API;
+// window.RW_CONFIG.API_BASE = RW_REMOTE_API;
