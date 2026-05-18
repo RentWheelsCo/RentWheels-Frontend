abf1aee3e9536b6dac3fb7360690cd200260adb5
Jinisha Shrestha <jinishashr7@gmail.com>
2026-05-18 22:01:27 +0545

    fix: add remote api endpoint

--- js/config.js
+++ js/config.js
@@ -2,4 +2,6 @@
 window.RW_CONFIG = window.RW_CONFIG || {};
 
 const RW_LOCAL_API = "http://localhost:5000/api";
-// const RW_REMOTE_API = "https://rentwheels-zr2p.onrender.com/api";
+const RW_REMOTE_API = "https://rentwheels-zr2p.onrender.com/api";
+
+window.RW_CONFIG.API_BASE = RW_REMOTE_API;
